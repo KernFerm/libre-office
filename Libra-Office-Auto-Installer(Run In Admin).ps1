3afe3aa7413ffa3b7f5878220bbbb2f840b8f7b9
bubbles the dev <152947339+KernFerm@users.noreply.github.com>
2024-08-22 20:44:39 -0400

    Update Libra-Office-Auto-Installer(Run In Admin).ps1
    Signed-off-by: bubbles the dev <152947339+KernFerm@users.noreply.github.com>

--- Libra-Office-Auto-Installer(Run In Admin).ps1
+++ Libra-Office-Auto-Installer(Run In Admin).ps1
@@ -19,7 +19,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 # Define URLs for software installers
-$LibreOfficeURL = 'https://tdf.mirror.garr.it/libreoffice/stable/24.2.4/win/x86_64/LibreOffice_24.2.4_Win_x86-64.msi'
+$LibreOfficeURL = 'https://tdf.mirror.garr.it/libreoffice/stable/24.8.0/win/x86_64/LibreOffice_24.8.0_Win_x86-64.msi'
 
 # Define installation paths (adjust as needed)
 $installPathLibreOffice = 'C:\Program Files\LibreOffice'
@@ -30,7 +30,7 @@ Set-Location -Path $PSScriptRoot
 # Downloading the LibreOffice installer
 try {
     Write-Log 'Downloading LibreOffice installer...'
-    Invoke-WebRequest -Uri $LibreOfficeURL -OutFile 'LibreOffice_24.2.4_Win_x86-64.msi' -ErrorAction Stop
+    Invoke-WebRequest -Uri $LibreOfficeURL -OutFile 'LibreOffice_24.8.0_Win_x86-64.msi' -ErrorAction Stop
     Write-Log 'LibreOffice installer downloaded successfully.'
 } catch {
     Write-Log "Error downloading LibreOffice installer: $_" "ERROR"
@@ -40,7 +40,7 @@ try {
 # Installing LibreOffice
 try {
     Write-Log 'Installing LibreOffice...'
-    Start-Process 'msiexec.exe' -ArgumentList "/i LibreOffice_24.2.4_Win_x86-64.msi /qn INSTALLDIR=`"$installPathLibreOffice`"" -NoNewWindow -Wait
+    Start-Process 'msiexec.exe' -ArgumentList "/i LibreOffice_24.8.0_Win_x86-64.msi /qn INSTALLDIR=`"$installPathLibreOffice`"" -NoNewWindow -Wait
     Write-Log 'LibreOffice installation initiated.'
 } catch {
     Write-Log "Error starting LibreOffice installation: $_" "ERROR"
@@ -58,7 +58,7 @@ if (Get-Command "$installPathLibreOffice\program\soffice.exe" -ErrorAction Silen
 # Delete the downloaded installer after installation
 try {
     Write-Log 'Deleting LibreOffice installer...'
-    Remove-Item 'LibreOffice_24.2.4_Win_x86-64.msi' -Force -ErrorAction Stop
+    Remove-Item 'LibreOffice_24.8.0_Win_x86-64.msi' -Force -ErrorAction Stop
     Write-Log 'Installer deleted. Installation process is complete.'
 } catch {
     Write-Log "Error deleting LibreOffice installer: $_" "ERROR"
